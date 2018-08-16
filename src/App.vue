7ccca9fcd33da2fd9f08bf3ec87999cecb971a27
Fredxingxing <33107804+Fredxingxing@users.noreply.github.com>
2018-08-16 23:55:46 +0800

    npm install 解决not found

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/Home">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/Home"></router-link>
     </div>
     <router-view/>
   </div>
@@ -17,16 +16,16 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+/*#nav {*/
+  /*padding: 30px;*/
+  /*a {*/
+    /*font-weight: bold;*/
+    /*color: #2c3e50;*/
+    /*&.router-link-exact-active {*/
+      /*color: #42b983;*/
+    /*}*/
+  /*}*/
+/*}*/
 html {
   font-size: 13.333333333333334vw; /* 100vw / 750 * 100 */
 }
